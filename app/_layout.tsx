d4c4a072335d46e47c07336986df6e1b825d86cb
Shai464 <shai@elector.co.il>
2024-11-26 13:35:05 +0200

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,29 @@
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Drawer} from 'expo-router/drawer';
+import { useEffect } from 'react';
+import { useRouter } from 'expo-router';
+
+export default function Layout() {
+    const router = useRouter();
+    const INITIAL_PAGE = '';
+
+
+    useEffect(() => {
+        router.push(INITIAL_PAGE);
+    }, []);
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={{drawerLabel: 'Dashboard', title: 'Dashboard'}}
+                />
+                <Drawer.Screen
+                    name="settings"
+                    options={{drawerLabel: 'Settings', title: 'Settings Title'}}
+                />
+
+            </Drawer>
+        </GestureHandlerRootView>
+    );
+}
